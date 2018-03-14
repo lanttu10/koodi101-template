672b660334b3fc15e48569b528ef4ca8a53a0a59
Aleksander Heino <sasha.heino@live.fi>
2018-03-14 15:30:22 +0200

    Lisää

--- backend/src/controllers/chats.js
+++ backend/src/controllers/chats.js
@@ -16,7 +16,12 @@ exports.list = async (ctx) => {
 exports.create = async (ctx) => {
   const params = ctx.request.body;
 
-  const chat = await database.Chat.create({message: params.message});
+  const chat = await database.Chat.create({
+    createdAt: this.createdAt,
+    temperature: this.temperature,
+    pressure: this.pressure,
+    brightness: this.brightness
+  });
 
   ctx.body = await chat.toJSON();
   ctx.status = 201;
